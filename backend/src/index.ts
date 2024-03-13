86f57a37d8d6c343b1466271de52c00f1afe7bdd
Mehmet Emin Avcıl <mehmeteminavcill@gmail.com>
2024-03-13 23:21:33 +0300

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,24 @@
+import  express, { Request, Response }  from "express";
+import cors from "cors"
+import "dotenv/config"
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+    .then(()=> console.log("Connected to database!"))
+
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/test",async (req:Request,res:Response)=>{
+    res.json({message:"Hello!"})
+})
+
+
+const PORT = 7000
+
+app.listen(PORT,()=>{
+    console.log("server started on localhost" , PORT)
+})
