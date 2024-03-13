7362bd92451593ca1812ff498002335ab85f7b28
Mehmet Emin Avcıl <mehmeteminavcill@gmail.com>
2024-03-14 02:13:30 +0300

    added frontend: query api for creating user , backend:createuser api- model,route,controller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,8 @@ import cors from "cors"
 import "dotenv/config"
 import mongoose from "mongoose";
 
+import myUserRoute from "./routes/MyUserRoute"
+
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
     .then(()=> console.log("Connected to database!"))
 
@@ -12,10 +14,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get("/test",async (req:Request,res:Response)=>{
-    res.json({message:"Hello!"})
-})
-
+app.use("/api/my/user",myUserRoute)
 
 const PORT = 7000
 
