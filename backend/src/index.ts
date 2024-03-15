5a2a1f8a3c0c32399b472cb49424318fcd0c68eb
Mehmet Emin Avcıl <mehmeteminavcill@gmail.com>
2024-03-15 15:23:06 +0300

    first deploy sttings

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,23 +1,27 @@
-import  express, { Request, Response }  from "express";
-import cors from "cors"
-import "dotenv/config"
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
 import mongoose from "mongoose";
 
-import myUserRoute from "./routes/MyUserRoute"
+import myUserRoute from "./routes/MyUserRoute";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
-    .then(()=> console.log("Connected to database!"))
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database!"));
 
+const app = express();
 
-const app = express()
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
 
-app.use("/api/my/user",myUserRoute)
+app.use("/api/my/user", myUserRoute);
 
-const PORT = 7000
+const PORT = 7000;
 
-app.listen(PORT,()=>{
-    console.log("server started on localhost" , PORT)
-})
+app.listen(PORT, () => {
+  console.log("server started on localhost", PORT);
+});
